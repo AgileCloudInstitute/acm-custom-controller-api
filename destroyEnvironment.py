8b42a1a61066021b0de5a5258780b541d517e903
AgileCloudInstitute <Agile.Cloud.Institute@GreenRiverIT.com>
2023-01-27 13:04:43 -0800

    more upgrades

--- destroyEnvironment.py
+++ destroyEnvironment.py
@@ -1,8 +1,24 @@
 import os
 import shutil
+import subprocess
+import re
+
+ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+
+def getSingleLineShellOutput(commandToRun):
+  proc = subprocess.Popen( commandToRun,cwd=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+  while True:
+    line = proc.stdout.readline()
+    if line:
+      thetext=line.decode('utf-8').rstrip('\r|\n')
+      decodedline=ansi_escape.sub('', thetext)
+      return decodedline
+    else:
+      break
 
 root_dir = r'venv'
 
+print("About to destroy virtual environment.")
 #Destroy the virtual environment by destroying the venv directory recursively
 for root, dirs, files in os.walk(root_dir):
 
@@ -13,3 +29,13 @@ for root, dirs, files in os.walk(root_dir):
             print(os.path.join(root, name))
             shutil.rmtree(os.path.join(root, name))
     os.rmdir(root_dir)
+print("Finished destroying virtual environment.  Next, we will destroy the twistd process that runs the custom controller API.")
+powershellLocation = getSingleLineShellOutput("where powershell")
+#powershellLocation = subprocess.call("where powershell", shell=True)
+print("powershellLocation is: ", powershellLocation)
+stopTwistdCommand = str(powershellLocation) + str(' Stop-Process -Name "twistd"')
+print("stopTwistdCommand is: ", stopTwistdCommand)
+subprocess.call(stopTwistdCommand, shell=True)
+print("The twistd process should be stopped now, which means that the custom controller should also be stopped.")
+
+
--- setupEnvironment.py
+++ setupEnvironment.py
@@ -12,7 +12,17 @@ def runShellCommand(commandToRun):
       thetext=line.decode('utf-8').rstrip('\r|\n')
       decodedline=ansi_escape.sub('', thetext)
       print(decodedline)
-        
+    else:
+      break
+
+def getSingleLineShellOutput(commandToRun):
+  proc = subprocess.Popen( commandToRun,cwd=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+  while True:
+    line = proc.stdout.readline()
+    if line:
+      thetext=line.decode('utf-8').rstrip('\r|\n')
+      decodedline=ansi_escape.sub('', thetext)
+      return decodedline
     else:
       break
 
@@ -31,9 +41,40 @@ runShellCommand("pip install Twisted")
 os.environ['PYTHONPATH'] = '.'
 print("Done updating PYTHONPATH.  About to start server.")
 
+twistdLocation = getSingleLineShellOutput("where twistd")
+#createServiceCommand = 'sc create TwistedFlaskApp binPath= "C:\\Users\\user8\\AppData\\Local\\Programs\\Python\\Python310\\Scripts\\twistd.exe web --wsgi --customControllerAPI.app"'
+#createServiceCommand = 'sc create TwistedFlaskApp binPath= "'+twistdLocation.replace("\\","\\\\")+' web --wsgi customControllerAPI.app"'
+#runShellCommand(createServiceCommand)
+#runShellCommand("sc start TwistedFlaskApp")
+#print("twistdLocation is: ", twistdLocation.replace("\\","\\\\"))
+
 ##Start the Twisted web server and configure it to control the api
-runShellCommand("twistd web --wsgi customControllerAPI.app")
-runShellCommand("twistd -n web --port tcp:8080 --wsgi customControllerAPI.app")
+#runShellCommand("twistd web --wsgi customControllerAPI.app")
+powershellLocation = getSingleLineShellOutput("where powershell")
+print("powershellLocation is: ", powershellLocation)
+#startTwistdCommand = powershellLocation + " twistd web --wsgi customControllerAPI.app &"
+#Next line says it started job but the api cannot be accessed.
+#startTwistdCommand = powershellLocation + " Start-Job { "+twistdLocation+" web --wsgi customControllerAPI.app }"
+
+#"$a = start-process -NoNewWindow powershell {timeout 10; 'done'} -PassThru"
+#Next line works, but the problem is the process persists after the venv is destroyed.
+startTwistdCommand = powershellLocation + " $a = start-process -NoNewWindow powershell { "+twistdLocation+" web --wsgi customControllerAPI.app"+" } -PassThru"
+print("startTwistdCommand is: ", startTwistdCommand)
+#subprocess.call('C:\Windows\System32\powershell.exe Get-Process', shell=True)
+subprocess.call(startTwistdCommand, shell=True)
+print("startTwistdCommand should be running in the background now.")
+# You can list all running processes with Get-Process
+#Get-Process twistd
+#Stop-Process -Name "twistd"
 #Note: The terminal may look in a held-up state after these commands because 
 #twstd is running in the foreground without output.  To confirm the api is running, 
 #you can run the makeApiCall.py program.
+
+#sc.exe [<servername>] create [<servicename>] [type= {own | share | kernel | filesys | rec | interact type= {own | share}}] [start= {boot | system | auto | demand | disabled | delayed-auto}] [error= {normal | severe | critical | ignore}] [binpath= <binarypathname>] [group= <loadordergroup>] [tag= {yes | no}] [depend= <dependencies>] [obj= {<accountname> | <objectname>}] [displayname= <displayname>] [password= <password>]
+
+#sc.exe create customControllerAPI start= demand [binpath= <binarypathname>] [group= <loadordergroup>] [tag= {yes | no}] [depend= <dependencies>] [obj= {<accountname> | <objectname>}] [displayname= <displayname>] [password= <password>]
+
+#sc create PythonApp binPath= "C:\Python34\Python.exe --C:\tmp\pythonscript.py"
+
+
+#C:\Users\user8\AppData\Local\Programs\Python\Python310\Scripts\twistd.exe
